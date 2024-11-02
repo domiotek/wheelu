c12121a9d912e272d79e1d116450e25f4b5e310b
Damian <41295244+MrDanixx@users.noreply.github.com>
2024-11-02 13:02:15 +0100

    Invalidate all queries on logout

--- WheeluFront/src/portals/LogoutPortal.tsx
+++ WheeluFront/src/portals/LogoutPortal.tsx
@@ -1,18 +1,17 @@
 import { useQueryClient } from "@tanstack/react-query";
-import { useLayoutEffect } from "react"
+import { useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function LogoutPortal() {
-
 	const queryClient = useQueryClient();
 	const navigate = useNavigate();
 
-	useLayoutEffect(()=>{
-		queryClient.invalidateQueries({queryKey: ["User"]});
+	useLayoutEffect(() => {
+		queryClient.invalidateQueries();
 		localStorage.removeItem("token");
 
 		navigate("/login");
-	},[]);
+	}, []);
 
-	return (<></>);
+	return <></>;
 }
